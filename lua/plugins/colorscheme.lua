33f2224591e2d2b46c3544649790678bbf02c4d0
ThaiDuy12345 <nguyenngocthaiduy.nt@gmail.com>
2023-12-24 13:47:05 +0700

    update nvim config

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -12,7 +12,16 @@ return {
   {
     "rmehri01/onenord.nvim",
     config = function()
-      vim.cmd.colorscheme "onenord"
+      --vim.cmd.colorscheme "onenord"
+    end
+  },
+  {
+    'AlexvZyl/nordic.nvim',
+    lazy = false,
+    priority = 1000,
+    config = function()
+      require 'nordic' .load()
+      vim.cmd.colorscheme("nordic")
     end
   }
 }
