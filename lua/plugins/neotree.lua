b30bfea43a8126dcf8fcc8e3ab16033c6639ef65
ThaiDuy12345 <nguyenngocthaiduy.nt@gmail.com>
2023-12-25 10:13:40 +0700

    update nvim config

--- lua/plugins/neotree.lua
+++ lua/plugins/neotree.lua
@@ -28,7 +28,7 @@ return {
         "filesystem",
         "buffers",
         "git_status",
-        -- "document_symbols",
+        "document_symbols",
       },
       add_blank_line_at_top = true, -- Add a blank line at the top of the tree.
       auto_clean_after_session_restore = false, -- Automatically clean up broken neo-tree buffers saved in sessions
@@ -57,7 +57,7 @@ return {
       log_level = "info", -- "trace", "debug", "info", "warn", "error", "fatal"
       log_to_file = false, -- true, false, "/path/to/file.log", use :NeoTreeLogs to show the file
       open_files_in_last_window = true, -- false = open files in top left window
-      open_files_do_not_replace_types = { "terminal", "Trouble", "qf", "edgy" }, -- when opening files, do not use windows containing these filetypes or buftypes
+      open_files_do_not_replace_types = { "Trouble", "qf", "edgy" }, -- when opening files, do not use windows containing these filetypes or buftypes
       popup_border_style = "NC", -- "double", "none", "rounded", "shadow", "single" or "solid"
       resize_timer_interval = 500, -- in ms, needed for containers to redraw right aligned and faded content
       -- set to -1 to disable the resize timer entirely
@@ -69,19 +69,20 @@ return {
       -- source_selector provides clickable tabs to switch between sources.
       source_selector = {
         winbar = true, -- toggle to show selector on winbar
-        statusline = true, -- toggle to show selector on statusline
+        statusline = false,
         show_scrolled_off_parent_node = true, -- this will replace the tabs with the parent path
         -- of the top visible node when scrolled down.
         sources = {
           { source = "filesystem" },
           { source = "buffers" },
           { source = "git_status" },
+          { source = "document_symbols" }
         },
-        content_layout = "end", -- only with `tabs_layout` = "equal", "focus"
+        content_layout = "center", -- only with `tabs_layout` = "equal", "focus"
         --                start  : |/ 󰓩 bufname     \/...
         --                end    : |/     󰓩 bufname \/...
         --                center : |/   󰓩 bufname   \/...
-        tabs_layout = "equal", -- start, end, center, equal, focus
+        tabs_layout = "center", -- start, end, center, equal, focus
         --             start  : |/  a  \/  b  \/  c  \            |
         --             end    : |            /  a  \/  b  \/  c  \|
         --             center : |      /  a  \/  b  \/  c  \      |
@@ -91,14 +92,14 @@ return {
         tabs_min_width = nil, -- nil | int: if int padding is added based on `content_layout`
         tabs_max_width = nil, -- this will truncate text even if `text_trunc_to_fit = false`
         padding = 0, -- can be int or table
-        -- padding = { left = 2, right = 0 },
+        --padding = { left = 0, right = 0 },
         -- separator = "▕", -- can be string or table, see below
-        separator = { left = "▏", right= "▕" },
+        --separator = { left = "▏", right= "▕" },
         -- separator = { left = "/", right = "\\", override = nil },     -- |/  a  \/  b  \/  c  \...
         -- separator = { left = "/", right = "\\", override = "right" }, -- |/  a  \  b  \  c  \...
         -- separator = { left = "/", right = "\\", override = "left" },  -- |/  a  /  b  /  c  /...
         -- separator = { left = "/", right = "\\", override = "active" },-- |/  a  / b:active \  c  \...
-        -- separator = "|",                                              -- ||  a  |  b  |  c  |...
+        separator = "",                                              -- ||  a  |  b  |  c  |...
         separator_active = nil, -- set separators around the active tab. nil falls back to `source_selector.separator`
         show_separator_on_edge = true,
         --                       true  : |/    a    \/    b    \/    c    \|
