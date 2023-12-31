aa8a9fef33dba966e3c643fcb7179dbda745e730
ThaiDuy12345 <nguyenngocthaiduy.nt@gmail.com>
2023-12-31 17:46:22 +0700

    update nvim config

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -1,3 +1,4 @@
+-- Vim basic configuration options
 local options = {
   "set autoindent",
   "set smartindent",
@@ -24,3 +25,25 @@ vim.keymap.set('n', '<C-o>', '')
 vim.keymap.set('n', '<leader>nhl', ':nohlsearch<CR>')
 vim.keymap.set('n', '<C-z>', '')
 vim.opt.termguicolors = true
+
+-- Vim terminal configuration keymap
+local function navigateToTerminal()
+  local currentBufNr = vim.api.nvim_get_current_buf()
+  local termBufNr = nil
+
+  for _, buf in ipairs(vim.api.nvim_list_bufs()) do
+    if vim.api.nvim_buf_get_option(buf, 'buftype') == 'terminal' then
+      termBufNr = buf
+      break
+    end
+  end
+
+  if termBufNr and currentBufNr == termBufNr then
+    vim.api.nvim_command('BufferClose!')
+  elseif termBufNr then
+    vim.api.nvim_command('buffer ' .. termBufNr)
+  else
+    vim.api.nvim_command("term")
+  end
+end
+vim.keymap.set('n', '<leader>t', function() navigateToTerminal() end)
