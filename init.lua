6b3f46624d420de7165a28e8570283a03c399aef
ThaiDuy12345 <nguyenngocthaiduy.nt@gmail.com>
2024-02-11 02:53:28 +0700

    update nvim

--- init.lua
+++ init.lua
@@ -14,3 +14,21 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 require("lazy").setup("plugins")
+
+-- Should install win32yank via scoop
+-- Clipboard integration for Neovim on WSL
+if vim.fn.has('win32') and vim.fn.has('unix') then
+    vim.g.clipboard = {
+        name = 'win32yank-wsl',
+        copy = {
+            ['+'] = 'win32yank.exe -i --crlf',
+            ['*'] = 'win32yank.exe -i --crlf',
+        },
+        paste = {
+            ['+'] = 'win32yank.exe -o --lf',
+            ['*'] = 'win32yank.exe -o --lf',
+        },
+        cache_enabled = 0,
+    }
+end
+
