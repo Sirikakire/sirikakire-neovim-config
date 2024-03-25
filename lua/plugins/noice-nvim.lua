85fe25fb3f4fa10f2c224e224928e2ce36db6384
ThaiDuy12345 <nguyenngocthaiduy.nt@gmail.com>
2024-03-25 17:29:37 +0700

    Update nvim

--- lua/plugins/noice-nvim.lua
+++ lua/plugins/noice-nvim.lua
@@ -5,7 +5,7 @@ return {
     -- "rcarriga/nvim-notify",
   },
   config = function()
-    --[[ require("noice").setup({
+    require("noice").setup({
       -- you can enable a preset for easier configuration
       presets = {
         bottom_search = false, -- use a classic bottom cmdline for search
@@ -198,24 +198,6 @@ return {
       status = {}, --- @see section on statusline components
       ---@type NoiceFormatOptions
       format = {}, --- @see section on formatting
-    }) ]]
-    require("noice").setup({
-      lsp = {
-        -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
-        override = {
-          ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
-          ["vim.lsp.util.stylize_markdown"] = true,
-          ["cmp.entry.get_documentation"] = true, -- requires hrsh7th/nvim-cmp
-        },
-      },
-      -- you can enable a preset for easier configuration
-      presets = {
-        bottom_search = false, -- use a classic bottom cmdline for search
-        command_palette = true, -- position the cmdline and popupmenu together
-        long_message_to_split = true, -- long messages will be sent to a split
-        inc_rename = false, -- enables an input dialog for inc-rename.nvim
-        lsp_doc_border = true, -- add a border to hover docs and signature help
-      },
     })
   end
 }
