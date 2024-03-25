82b13f7a43234b0e154754438a29eedf73af4124
ThaiDuy12345 <nguyenngocthaiduy.nt@gmail.com>
2024-03-25 18:02:05 +0700

    Update nvim

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -51,25 +51,3 @@ vim.api.nvim_create_autocmd("TextYankPost", {
     vim.highlight.on_yank()
   end
 })
--- vim.cmd('au TermOpen * au <buffer> WinEnter redraw!')
--- vim.cmd('autocmd ColorScheme * highlight Normal ctermbg=NONE guibg=NONE')
--- Vim terminal configuration keymap
---[[ local function navigateToTerminal()
-  local currentBufNr = vim.api.nvim_get_current_buf()
-  local termBufNr = nil
-
-  for _, buf in ipairs(vim.api.nvim_list_bufs()) do
-    if vim.api.nvim_buf_get_option(buf, 'buftype') == 'terminal' then
-      termBufNr = buf
-      break
-    end
-  end
-  if termBufNr and currentBufNr == termBufNr then
-    vim.api.nvim_command('BufferClose!')
-  elseif termBufNr then
-    vim.api.nvim_command('buffer ' .. termBufNr)
-  else
-    vim.api.nvim_command("term")
-  end
-end
-vim.keymap.set('n', '<leader>t', function() navigateToTerminal() end) ]]
