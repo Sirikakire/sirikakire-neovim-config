228dc6c1af76e914337c4d92117956a4c28c4d4b
ThaiDuy12345 <nguyenngocthaiduy.nt@gmail.com>
2024-04-08 01:17:14 +0700

    Update nvim

--- lua/plugins/noice-nvim.lua
+++ lua/plugins/noice-nvim.lua
@@ -6,14 +6,6 @@ return {
   },
   config = function()
     require("noice").setup({
-      -- you can enable a preset for easier configuration
-      presets = {
-        bottom_search = false, -- use a classic bottom cmdline for search
-        command_palette = false, -- position the cmdline and popupmenu together
-        long_message_to_split = true, -- long messages will be sent to a split
-        inc_rename = false, -- enables an input dialog for inc-rename.nvim
-        lsp_doc_border = true, -- add a border to hover docs and signature help
-      },
       cmdline = {
         enabled = true, -- enables the Noice cmdline UI
         view = "cmdline", -- view for rendering the cmdline. Change to `cmdline` to get a classic cmdline at the bottom
@@ -107,15 +99,10 @@ return {
         -- event is always "notify" and kind can be any log level as a string
         -- The default routes will forward notifications to nvim-notify
         -- Benefit of using Noice for this is the routing and consistent history view
-        enabled = false,
+        enabled = true,
         view = "notify",
       },
       lsp = {
-        override = {
-          ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
-          ["vim.lsp.util.stylize_markdown"] = true,
-          ["cmp.entry.get_documentation"] = true, -- requires hrsh7th/nvim-cmp
-        },
         progress = {
           enabled = true,
           -- Lsp Progress is formatted using the builtins for lsp_progress. See config.format.builtin
@@ -127,6 +114,14 @@ return {
           throttle = 1000 / 30, -- frequency to update lsp progress message
           view = "mini",
         },
+        override = {
+          -- override the default lsp markdown formatter with Noice
+          ["vim.lsp.util.convert_input_to_markdown_lines"] = false,
+          -- override the lsp markdown formatter with Noice
+          ["vim.lsp.util.stylize_markdown"] = false,
+          -- override cmp documentation with Noice (needs the other options to work)
+          ["cmp.entry.get_documentation"] = false,
+        },
         hover = {
           enabled = false,
           silent = true, -- set to true to not show a message if hover is not available
@@ -137,9 +132,9 @@ return {
         signature = {
           enabled = false,
           auto_open = {
-            enabled = true,
-            trigger = true, -- Automatically show signature help when typing a trigger character from the LSP
-            luasnip = true, -- Will open signature help when jumping to Luasnip insert nodes
+            enabled = false,
+            trigger = false, -- Automatically show signature help when typing a trigger character from the LSP
+            luasnip = false, -- Will open signature help when jumping to Luasnip insert nodes
             throttle = 50, -- Debounce lsp signature help request by 50ms
           },
           view = nil, -- when nil, use defaults from documentation
@@ -189,9 +184,18 @@ return {
         excluded_filetypes = { "cmp_menu", "cmp_docs", "notify" },
       },
       ---@type NoicePresets
+      presets = {
+        -- you can enable a preset by setting it to true, or a table that will override the preset config
+        -- you can also add custom presets that you can enable/disable with enabled=true
+        bottom_search = false, -- use a classic bottom cmdline for search
+        command_palette = false, -- position the cmdline and popupmenu together
+        long_message_to_split = false, -- long messages will be sent to a split
+        inc_rename = false, -- enables an input dialog for inc-rename.nvim
+        lsp_doc_border = true, -- add a border to hover docs and signature help
+      },
       throttle = 1000 / 30, -- how frequently does Noice need to check for ui updates? This has no effect when in blocking mode.
       ---@type NoiceConfigViews
-      views = {},
+      views = {}, ---@see section on views
       ---@type NoiceRouteConfig[]
       routes = {}, --- @see section on routes
       ---@type table<string, NoiceFilter>
--- lua/plugins/nvim-notify.lua
+++ lua/plugins/nvim-notify.lua
@@ -1,28 +1,28 @@
 return {
   'rcarriga/nvim-notify',
   config = function()
-    require("notify").setup({
-      -- background_colour = "Normal",
-      background_colour = "#000000",
-      fps = 60,
-      icons = {
-        DEBUG = " ",
-        ERROR = " ",
-        INFO = " ",
-        TRACE = " ✎",
-        WARN = " "
-      },
-      -- level = 2,
-      minimum_width = 50,
-      render = "compact",
-      stages = "fade",
-      time_formats = {
-        notification = "%T",
-        notification_history = "%FT%T"
-      },
-      timeout = 3000,
-      top_down = true
-    })
-    vim.notify = require('notify')
-  end
+		require("notify").setup({
+			-- background_colour = "Normal",
+			background_colour = "#000000",
+			fps = 60,
+			icons = {
+				DEBUG = " ",
+				ERROR = " ",
+				INFO = " ",
+				TRACE = " ✎",
+				WARN = " "
+			},
+			-- level = 0,
+			minimum_width = 50,
+			render = "compact",
+			stages = "fade",
+			time_formats = {
+				notification = "%T",
+				notification_history = "%FT%T"
+			},
+			timeout = 2000,
+			top_down = true
+		})
+		vim.notify = require('notify')
+	end
 }
