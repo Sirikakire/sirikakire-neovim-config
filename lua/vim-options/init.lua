c0bd97e28c3f3b0bc103827778d75662ed2efb2b
ThaiDuy12345 <nguyenngocthaiduy.nt@gmail.com>
2024-04-09 20:22:02 +0700

    Update nvim

--- lua/vim-options/init.lua
+++ lua/vim-options/init.lua
@@ -2,7 +2,7 @@ require(".vim-options.init_default_value").setup({
   win_separator = false,
   syn_all_border_color = true,
   syn_all_telescope_border = false,
-  transparent_background = false,
+  transparent_background = true,
 })
 
 require(".vim-options.autocmd")
