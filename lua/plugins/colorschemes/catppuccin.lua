9845ac17cc850d834e2162e8076409aa1892427d
ThaiDuy12345 <nguyenngocthaiduy.nt@gmail.com>
2024-05-11 18:35:35 +0700

    update nvim

--- lua/plugins/colorschemes/catppuccin.lua
+++ lua/plugins/colorschemes/catppuccin.lua
@@ -2,8 +2,7 @@ return vim.b.colorscheme ~= "catppuccin" and {} or {
   "catppuccin/nvim",
   priority = 100,
   config = function()
-    require("catppuccin").setup(require("plugins.colorschemes.catppuccin-colorscheme.thanhvule"))
-    vim.cmd.colorscheme "catppuccin-mocha"
+    require("catppuccin").setup(require("plugins.colorschemes.catppuccin-colorscheme.default"))
+    vim.cmd.colorscheme "catppuccin-frappe"
   end
 }
-
