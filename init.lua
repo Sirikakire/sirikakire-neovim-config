a87cd5adf1963dd12bf70d475be155f67678384e
ThaiDuy12345 <nguyenngocthaiduy.nt@gmail.com>
2024-07-09 18:09:38 +0700

    Update: nvim

--- init.lua
+++ init.lua
@@ -13,11 +13,11 @@ require("init").setup({
   float_window_brightness = 0,
   better_cmp_cursor_line = true,
   line_space = -1,
-  colorscheme = "tokyonight",
+  colorscheme = "everforest",
   neovide_setting = {
     transparency = 1,
     font_setting = {
-      name = "CaskaydiaCove Nerd Font",
+      name = "JetBrainsMonoNL Nerd Font",
       size = 13,
       bold = false,
     }
