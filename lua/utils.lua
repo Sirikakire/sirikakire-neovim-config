53400da30f19347c77748a444ddb20360bbd0d44
ThaiDuy12345 <nguyenngocthaiduy.nt@gmail.com>
2024-10-02 17:14:00 +0700

    Update: nvim

--- lua/utils.lua
+++ lua/utils.lua
@@ -311,7 +311,7 @@ U.kind_icons = {
 
 -- NOTE: Lsp server list
 U.lsp_servers = {
-  "ts_ls",                        -- typescript
+  "ts_ls",                           -- typescript
   "lua_ls",                          -- lua
   "cssls",                           -- css
   "jsonls",                          -- json
