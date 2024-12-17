b741f68c6981f5c8b32986030155577925991ef9
ThaiDuy12345 <nguyenngocthaiduy.nt@gmail.com>
2024-12-17 18:18:10 +0700

    Update nvim

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -8,11 +8,30 @@ return {
   event = "VeryLazy",
   opts = function()
     local actions = require("telescope.actions")
+    local action_state = require('telescope.actions.state')
     local common_setting = {
       -- theme = "dropdown",
     }
     require("telescope").load_extension("ui-select")
 
+    local select_one_or_multi = function(prompt_bufnr)
+      local picker = require('telescope.actions.state').get_current_picker(prompt_bufnr)
+      local multi = picker:get_multi_selection()
+      if not vim.tbl_isempty(multi) then
+        require('telescope.actions').close(prompt_bufnr)
+        for _, j in pairs(multi) do
+          if j.path ~= nil then
+            if j.lnum ~= nil then
+              vim.cmd(string.format("%s +%s %s", "edit", j.lnum, j.path))
+            else
+              vim.cmd(string.format("%s %s", "edit", j.path))
+            end
+          end
+        end
+      else
+        require('telescope.actions').select_default(prompt_bufnr)
+      end
+    end
     return {
       extensions = {
         ["ui-select"] = {
@@ -34,6 +53,7 @@ return {
         mappings = {
           i = {
             ["<esc>"] = actions.close,
+            ["<CR>"] = select_one_or_multi
           },
         },
         sorting_strategy = "ascending",
