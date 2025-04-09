15337e94bfc8537f973d4b93292c3de55206ad32
ThaiDuy12345 <nguyenngocthaiduy.nt@gmail.com>
2025-04-09 18:01:55 +0700

    Update: nvim

--- lua/plugins/avante-nvim.lua
+++ lua/plugins/avante-nvim.lua
@@ -4,8 +4,8 @@ return {
   version = false, -- Never set this value to "*"! Never!
   keys = require("keymap").avante_keymaps,
   opts = {
-    auto_suggestions_provider = nil,
-    cursor_applying_provider = nil,
+    auto_suggestions_provider = "copilot",
+    cursor_applying_provider = "copilot",
     provider = "copilot",
     copilot = {
       endpoint = "https://api.githubcopilot.com",
@@ -27,7 +27,7 @@ return {
       auto_focus_sidebar = true,
       auto_suggestions_respect_ignore = false,
       jump_result_buffer_on_finish = true,
-      use_cwd_as_project_root = false,
+      use_cwd_as_project_root = true,
       auto_suggestions = false, -- Experimental stage
       auto_set_highlight_group = true,
       auto_set_keymaps = true,
@@ -35,8 +35,8 @@ return {
       support_paste_from_clipboard = true,
       minimize_diff = true, -- Whether to remove unchanged lines when applying a code block
       enable_token_counting = false, -- Whether to enable token counting. Default to true.
-      enable_cursor_planning_mode = false, -- Whether to enable Cursor Planning Mode. Default to false.
-      enable_claude_text_editor_tool_mode = false, -- Whether to enable Claude Text Editor Tool Mode.
+      enable_cursor_planning_mode = true, -- Whether to enable Cursor Planning Mode. Default to false.
+      enable_claude_text_editor_tool_mode = true, -- Whether to enable Claude Text Editor Tool Mode.
     },
     history = {
       max_tokens = 0,
@@ -46,17 +46,24 @@ return {
         filename = "pasted-%Y-%m-%d-%H-%M-%S",
       },
     },
-    mappings = {},
+    mappings = {
+      suggestion = {
+        accept = "<tab>",
+        next = "<M-]>",
+        prev = "<M-[>",
+        dismiss = "<C-]>",
+      },
+    },
     hints = { enabled = true },
     windows = {
-      position = "top", -- the position of the sidebar
+      position = "right", -- the position of the sidebar
       wrap = true, -- similar to vim.o.wrap
       width = 40, -- default % based on available width
       height = 30,
       sidebar_header = {
-        enabled = true, -- true, false to enable/disable the header
+        enabled = false, -- true, false to enable/disable the header
         align = "right", -- left, center, right for title
-        rounded = false,
+        rounded = true,
       },
       input = {
         prefix = "> ",
