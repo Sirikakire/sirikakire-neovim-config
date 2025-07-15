3f8c9abb21ebdb33634c2f52943aa5523894bfbf
ThaiDuy12345 <nguyenngocthaiduy.nt@gmail.com>
2025-07-15 17:07:46 +0700

    Update: nvim

--- lua/plugins/avante-nvim.lua
+++ lua/plugins/avante-nvim.lua
@@ -15,7 +15,7 @@ return {
     providers = {
       copilot = {
         endpoint = "https://api.githubcopilot.com",
-        model = "claude-3.7-sonnet",
+        model = "claude-3.7-sonnet", -- Model to use, e.g., "claude-3-7-sonnet"
         proxy = nil, -- [protocol://]host[:port] Use this proxy
         allow_insecure = false, -- Allow insecure server connections
         timeout = 30000, -- Timeout in milliseconds
