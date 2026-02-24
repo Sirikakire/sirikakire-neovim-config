03bef76beb7b088fee996102e08330090c86ebc8
Sirikakire <thaiduynguyen.nt@gmail.com>
2026-02-24 16:56:15 +0700

    Update: nvim

--- lua/keymap.lua
+++ lua/keymap.lua
@@ -253,4 +253,12 @@ K.avante_keymaps = {
   { "<leader>af", nil },
 }
 
+-- NOTE: Setup keymap for yazi
+K.yazi_keymaps = {
+  -- 👇 in this section, choose your own keymappings!
+  { "<leader>-", mode = { "n", "v" }, "<cmd>Yazi<cr>", desc = "Open yazi at the current file" },
+  { "<leader>cw", "<cmd>Yazi cwd<cr>", desc = "Open the file manager in nvim's working directory" },
+  { "<c-up>", "<cmd>Yazi toggle<cr>", desc = "Resume the last yazi session" },
+}
+
 return K
