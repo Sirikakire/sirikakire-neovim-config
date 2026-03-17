e9f7ec1bfc45cca9fbf676a8cf9d1ebdb3dce7f8
Sirikakire <thaiduynguyen.nt@gmail.com>
2026-03-17 17:48:45 +0700

    Update: nvim

--- lua/keymap.lua
+++ lua/keymap.lua
@@ -256,9 +256,69 @@ K.avante_keymaps = {
 -- NOTE: Setup keymap for yazi
 K.yazi_keymaps = {
   -- 👇 in this section, choose your own keymappings!
-  { "<leader>-", mode = { "n", "v" }, "<cmd>Yazi<cr>", desc = "Open yazi at the current file" },
-  { "<leader>cw", "<cmd>Yazi cwd<cr>", desc = "Open the file manager in nvim's working directory" },
-  { "<c-up>", "<cmd>Yazi toggle<cr>", desc = "Resume the last yazi session" },
+  { "<leader>ec", mode = { "n", "v" }, "<cmd>Yazi<cr>", desc = "Open yazi at the current file" },
+  { "<leader>ew", "<cmd>Yazi cwd<cr>", desc = "Open the file manager in nvim's working directory" },
+}
+
+K.cursor_agent_keymaps = {
+  { "<leader>cca", mode = "v", "<CMD>CursorAgentSelection<CR>", desc = "Cursor Agent: Send selection" },
+  { "<leader>cca", mode = "n", "<CMD>CursorAgent<CR>", desc = "Cursor Agent: Toggle terminal" },
+  { "<leader>ccA", mode = "n", "<CMD>CursorAgentBuffer<CR>", desc = "Cursor Agent: Send buffer" },
+}
+
+K.sidekick_keymaps = {
+
+  {
+    "<tab>",
+    function()
+      -- if there is a next edit, jump to it, otherwise apply it if any
+      if not require("sidekick").nes_jump_or_apply() then
+        return "<Tab>" -- fallback to normal tab
+      end
+    end,
+    expr = true,
+    desc = "Goto/Apply Next Edit Suggestion",
+  },
+  {
+    "<leader>aa",
+    function() require("sidekick.cli").toggle() end,
+    desc = "Sidekick Toggle CLI",
+  },
+  {
+    "<leader>as",
+    function() require("sidekick.cli").select() end,
+    -- Or to select only installed tools:
+    -- require("sidekick.cli").select({ filter = { installed = true } })
+    desc = "Select CLI",
+  },
+  {
+    "<leader>ad",
+    function() require("sidekick.cli").close() end,
+    desc = "Detach a CLI Session",
+  },
+  {
+    "<leader>at",
+    function() require("sidekick.cli").send({ msg = "{this}" }) end,
+    mode = { "x", "n" },
+    desc = "Send This",
+  },
+  {
+    "<leader>af",
+    function() require("sidekick.cli").send({ msg = "{file}" }) end,
+    desc = "Send File",
+  },
+  {
+    "<leader>av",
+    function() require("sidekick.cli").send({ msg = "{selection}" }) end,
+    mode = { "x" },
+    desc = "Send Visual Selection",
+  },
+  {
+    "<leader>ap",
+    function() require("sidekick.cli").prompt() end,
+    mode = { "n", "x" },
+    desc = "Sidekick Select Prompt",
+  },
 }
 
 return K
---
+++ lua/plugins/cursor-agents.lua
@@ -0,0 +1,10 @@
+return {
+  -- "xTacobaco/cursor-agent.nvim",
+  -- keys = require("keymap").cursor_agent_keymaps,
+  -- opts = {
+  --   -- Executable or argv table. Example: "cursor-agent" or {"/usr/local/bin/cursor-agent"}
+  --   cmd = "cursor-agent",
+  --   -- Additional arguments always passed to the CLI
+  --   args = {},
+  -- },
+}
