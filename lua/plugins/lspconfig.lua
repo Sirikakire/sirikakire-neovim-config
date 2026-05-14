0e8f15bfaa63046746ef417fe0211650c2b81240
Sirikakire <thaiduynguyen.nt@gmail.com>
2026-05-14 16:53:00 +0700

    Update: nvim

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -42,7 +42,7 @@ return {
       -- })
 
       -- NOTE: Disabled LSP generate logging file
-      vim.lsp.set_level("off")
+      vim.lsp.log.set_level("off")
 
       -- NOTE: Mapping keymap for lsp
       vim.api.nvim_create_autocmd("LspAttach", {
